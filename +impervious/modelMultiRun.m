9e7d21bccadc5ce80457ae1be4c1e97d045e55f4
Lyman Gillispie <lyman.gillispie@gmail.com>
2013-08-29 10:14:51 -0800

    typo fix

--- +impervious/modelMultiRun.m
+++ +impervious/modelMultiRun.m
@@ -1,4 +1,15 @@
 function runs = modelMultiRun(modelpath, basefile, varargin)
+% function modelMultiRun
+%
+% Args: modelpath - fully qualified path to a model's executeable
+%       basefile - fully qualified path to a valid config file for the
+%       model, this will be modified based upon the the list of key-value
+%       pairs passed to varargin
+%       varargin - a list of key-value pairs which are modified, e.g.
+%           modelMultiRun('debam', 'input.txt', 'icekons', [5:0.1:6])
+%         will run the model with icekons set to each value in [5:0.1:6]
+% Returns: runs - a container.Maps indexed by hashes of HashedRun objects,
+%           each corresponding to a single model run.
 
 
 s = fileread(basefile);
@@ -26,7 +37,7 @@ for combo = 1:nCombs(1)
   
   runs(HR.hash) = HR;
   HR.runModel();
-  changefile = fopen([HR.outPath 'chages.txt'],'w');
+  changefile = fopen([HR.outPath 'changes.txt'],'w');
   fprintf(changefile,'%s', msg);
   fclose(changefile);
 end 
