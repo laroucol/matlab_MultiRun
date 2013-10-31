7a773dc563ec8e7b77fbac859338d58bfaab9a61
Lyman Gillispie <lyman.gillispie@gmail.com>
2013-10-31 11:40:55 -0400

    Comments to qualityToFile.m

--- +MultiRun/+quality/qualityToFile.m
+++ +MultiRun/+quality/qualityToFile.m
@@ -1,35 +1,43 @@
-function qualityToFile(runs, kw, combs, filename)
+function qualityToFile(runs, kw, combs, outfilename)
 %TODO: - choice of quality metrics
 %      - only filenames?
+%     - return value?
 
-
-fid = fopen(filename, 'w');
-% TODO: error out when file don't open
+%open the output file.
+fid = fopen(outfilename, 'w');
 if fid == -1
   error('MultiRun:quality:qualityToFile:fileError', 'Outputfile could not be opened.')
 end
 
-% get perfinfo
+%Figure out how many runs have taken place.
 nCombs = size(runs);
 nCombs = nCombs(1);
 
+%Extract model quality info, and figure out how many columns our file will
+%need.
 quality = MultiRun.quality.qualityFromRuns(runs);
 perfKeys = quality.keys;
 nKeys = length(perfKeys);
 nKw = length(kw);
 
-% format: hash [changed_values] [perfomance_metrics]
+%Row format: hash [perfomance_metrics] [changed_values] 
 head_fmt = ['%s\t' repmat('%s\t', 1, nKw + nKeys - 1) '%s\n'];
 body_fmt = ['%s\t' repmat('%g\t', 1, nKw + nKeys - 1) '%g\n'];
 
+%Write column titles to file
 fprintf(fid, head_fmt, 'Hash', perfKeys{:}, kw{:});
 
+%Helper function for refrencing elements of the keys of quality, which are
+%arrays
 subindex = @(M,r) M{r};
 
+%write each row of the output file
 for ii = 1:nCombs
+  %get the run's hash, and parameter constellation
   hash = runs{ii}.hash;
   changed = combs(ii,:);
   
+  %allocate performance array, then populate it with values from quality.
   perf = zeros(1, nKeys);
   for jj = 1:nKeys
     perf(jj) = subindex(quality(perfKeys{jj}), ii);
