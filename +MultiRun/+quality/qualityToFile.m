447034e6447eee812d5120f34a0ce04c816ff813
Lyman Gillispie <lyman.gillispie@gmail.com>
2013-10-31 11:22:11 -0400

    Renamed to imporve code ambiguity.

--- +MultiRun/+quality/qualityToFile.m
+++ +MultiRun/+quality/qualityToFile.m
@@ -14,15 +14,15 @@ nCombs = size(runs);
 nCombs = nCombs(1);
 
 quality = MultiRun.quality.qualityFromRuns(runs);
-keys = quality.keys;
-nKeys = length(keys);
+perfKeys = quality.keys;
+nKeys = length(perfKeys);
 nKw = length(kw);
 
 % format: hash [changed_values] [perfomance_metrics]
 head_fmt = ['%s\t' repmat('%s\t', 1, nKw + nKeys - 1) '%s\n'];
 body_fmt = ['%s\t' repmat('%g\t', 1, nKw + nKeys - 1) '%g\n'];
 
-fprintf(fid, head_fmt, 'Hash', keys{:}, kw{:});
+fprintf(fid, head_fmt, 'Hash', perfKeys{:}, kw{:});
 
 subindex = @(M,r) M{r};
 
@@ -32,7 +32,7 @@ for ii = 1:nCombs
   
   perf = zeros(1, nKeys);
   for jj = 1:nKeys
-    perf(jj) = subindex(quality(keys{jj}), ii);
+    perf(jj) = subindex(quality(perfKeys{jj}), ii);
   end
   
   fprintf(fid, body_fmt, hash, perf(:), changed(:));
